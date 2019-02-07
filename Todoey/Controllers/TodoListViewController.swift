44eebf483ea2d8fd12c0646f3e793806a3035eb6
Faiz Mohideen <faiz@rolleragency.co.uk>
2019-02-07 16:37:27 +0000

    244. How to Implement a UISearchBar and Querying with Core Data

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -80,14 +80,27 @@ class TodoListViewController: UITableViewController {
         self.tableView.reloadData()
     }
     
-    func loadItems() {
-        let request: NSFetchRequest<Item> = Item.fetchRequest()
-        
+    func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest()) {
         do {
            itemArray = try self.context.fetch(request)
         } catch {
             print("Error fetching data from context, \(error)")
         }
+        
+        self.tableView.reloadData()
+    }
+}
+
+extension TodoListViewController: UISearchBarDelegate {
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        let request: NSFetchRequest<Item> = Item.fetchRequest()
+        
+        request.predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+        
+        request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
+
+        loadItems(with: request)
     }
 }
 
